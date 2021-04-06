f17b0c0ebe12ec89f4c2d62ae0103ef5ea8a21a8
Francisco Morais <fgmorais18@gmail.com>
2021-04-05 22:47:20 -0300

    :sparkles: feat: add authentication to sign in

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,25 +1,28 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { signIn, signOut, useSession } from 'next-auth/client';
 
 import styles from './styles.module.scss';
 
-export function SignInButton() {
+export function SignInButton() {   
+   const [session] = useSession();
+   console.log(session);
 
-   const isUserLoggedIn = true;
-
-   return isUserLoggedIn ? (
+   return session ? (
       <button 
          type="button"
          className={styles.signInButton}
+         onClick={() => {signOut()}}
       >
          <FaGithub color="#04d361" />
-         Francisco Morais
+         {session.user.name}
          <FiX color="#737380" className={styles.closeIcon} />
       </button>
    ) : (
       <button 
          type="button"
          className={styles.signInButton}
+         onClick={() => signIn('github')}
       >
          <FaGithub color="#eba417" />
          Sign in with Github
